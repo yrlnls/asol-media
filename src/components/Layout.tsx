a69d9faa6d30cacf4993b94ba806b2bd2baa60b5
tillenotuoma <totuoma27@gmail.com>
2026-05-12 07:05:13 +0300

    linked in update

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -248,7 +248,7 @@ export default function Layout() {
             <div className="footer-copy">© 2026 Asol Media. All rights reserved.</div>
           </div>
           <div className="footer-social">
-            <a href="https://ke.linkedin.com/in/asol-media-1994ba227?trk=public_profile_browsemap" aria-label="LinkedIn" className="social-link social-linkedin" target="_blank" rel="noopener">
+            <a href="https://www.linkedin.com/in/asol-media🎥-b3641a30a?utm_source=share_via&utm_content=profile&utm_medium=member_android" aria-label="LinkedIn" className="social-link social-linkedin" target="_blank" rel="noopener">
               <svg viewBox="0 0 24 24" aria-hidden="true">
                 <path d="M4.98 3.5a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5zM3 9h4v12H3V9zm7 0h3.8v1.7h.1c.5-.9 1.8-1.9 3.6-1.9 3.9 0 4.6 2.5 4.6 5.8V21h-4v-5.2c0-1.3 0-3-1.9-3-1.9 0-2.2 1.4-2.2 2.9V21h-4V9z" />
               </svg>
